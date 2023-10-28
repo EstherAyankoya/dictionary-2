f019ccd4109f7bf6e9a6b18159e70ca0f77ed643
EstherAyankoya <estherayankoya@gmail.com>
2023-10-29 00:52:41 +0100

    diction

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 
 import './App.css';
-import Dictionary from "./Dictioary"
+import Dictionary from "./Dictionary"
 
 function App() {
   return (
---
+++ src/Dictionary.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./Dictionary.css";
+
+export default function Dictionary() {
+    let [keyword, setKeyword] = useState(props.defaultKeyword);
+    let [results, setResults] = useState(null);
+    let [loaded, setLoaded] = useState(false);
+    let [photos, setPhotos] = useState(null);
+}
+
+    
