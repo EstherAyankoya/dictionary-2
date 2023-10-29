5d8fd1e15d37285600d738f02269a18dc68fbd3b
EstherAyankoya <estherayankoya@gmail.com>
2023-10-29 01:00:28 +0100

    updated

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,11 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Dictionary.css";
 
 export default function Dictionary() {
-    let [keyword, setKeyword] = useState(props.defaultKeyword);
-    let [results, setResults] = useState(null);
-    let [loaded, setLoaded] = useState(false);
-    let [photos, setPhotos] = useState(null);
+  let [keyword, setKeyword] = useState("");
+
+  function search(event) {
+    event.preventDefault();
+    alert(`Searching for ${keyword} definition`);
+  }
+
+  function handleKeywordChange(event) {
+    setKeyword(event.target.value);
+  }
+
+  return (
+    <div className="Dictionary">
+      <form onSubmit={search}>
+        <input type="search" onChange={handleKeywordChange} />
+      </form>
+    </div>
+  );
 }
 
     
